5590c5ea16151d5ed2ea2d6ccdc9627d81705235
Darya Iwako <darya.iwako@gmail.com>
2024-08-27 11:55:37 -0700

    Renaming CSS files to SASS files, and importing them to client/index.js

--- evaluate-news-nlp/starter_project/src/client/index.js
+++ evaluate-news-nlp/starter_project/src/client/index.js
@@ -7,3 +7,9 @@ import { handleSubmit } from './js/formHandler'
 // console.log("CHANGE!!");
 
 // sass files
+
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
